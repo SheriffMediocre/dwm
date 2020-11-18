cf708f12eb1d7923fb1a8244fea0e1ffdf0651b0
SheriffMediocre <71788330+SheriffMediocre@users.noreply.github.com>
2020-11-17 20:29:47 -0500

    Add files via upload

--- config.h
+++ config.h
@@ -6,13 +6,13 @@
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static unsigned int snap      = 15;       /* snap pixel */
+static unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 5;       /* vert inner gap between windows */
 static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
@@ -20,7 +20,7 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
+static char selbordercolor[]        = "#177013";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
@@ -119,12 +119,12 @@ ResourcePref resources[] = {
 		{ "nmaster",		INTEGER, &nmaster },
 		{ "resizehints",	INTEGER, &resizehints },
 		{ "mfact",		FLOAT,	&mfact },
-		{ "gappih",		INTEGER, &gappih },
-		{ "gappiv",		INTEGER, &gappiv },
-		{ "gappoh",		INTEGER, &gappoh },
-		{ "gappov",		INTEGER, &gappov },
-		{ "swallowfloating",	INTEGER, &swallowfloating },
-		{ "smartgaps",		INTEGER, &smartgaps },
+		{ "gappih",		STRING, &gappih },
+		{ "gappiv",		STRING, &gappiv },
+		{ "gappoh",		STRING, &gappoh },
+		{ "gappov",		STRING, &gappov },
+		{ "swallowfloating",	STRING, &swallowfloating },
+		{ "smartgaps",		STRING, &smartgaps },
 };
 
 #include <X11/XF86keysym.h>
